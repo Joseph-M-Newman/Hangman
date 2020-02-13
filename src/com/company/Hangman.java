3109a38f0563931227eebcf69bd74c43649c7816
Joseph <Josephnew900@gmail.com>
2020-02-12 17:56:06 -0800

    Change to caps done; now just need to put spaces inbetween the the letters of the word + add the hint feature; then completed.

--- src/com/company/Hangman.java
+++ src/com/company/Hangman.java
@@ -16,23 +16,30 @@ import java.util.Scanner;
 public class Hangman {
 
 	public static void main(String[] args) {
+
 		String Hangman_Word = "";
 		int Guesses = 10;
 		Scanner input = new Scanner(System.in);
+
 		System.out.println("----------Welcome to Hangman!----------");
 		System.out.print("Please enter a word: ");
+
 		Hangman_Word = input.nextLine();
 		String newWord = Hangman_Word.replaceAll("\\S", "_");
+
 		System.out.println(newWord); // check if the word is being displayed properly; Not needed to
 		// always be displayed, for debugging;
+
 		HangMan(Hangman_Word + " ");
 	}
 	public static void HangMan(String Word) {
 		int Guesses = 5;
 		int counter = 0;
 		int hint = 2;
-		String Hangman_Word = Word;
+		//sets the word entered as a CAPITALS;
+		String Hangman_Word = Word.toUpperCase();
 		String newWord = Hangman_Word.replaceAll("\\S", "_");
+
 		//Final word is the guessing input
 		char[] FinalWord = newWord.toCharArray();
 		Scanner input = new Scanner(System.in);
@@ -51,16 +58,12 @@ public class Hangman {
 		String comp1 = new String(FinalWord);
 
 		while(active == true){
-			System.out.println(Guesses + " THIS IS THE COUNTER;");
-
+			//break if out of guesses;
 			if(Guesses == -1) {
-				active = false;
 				break;
 			}
+			//break if word is == to each other( the guesses word and the actual word)
 			if(comp1.equals(comp2)) {
-
-				System.out.println("I got here jrbfeqi");
-				active = false;
 				break;
 			}
 			if(Guesses >= 1){
@@ -69,7 +72,10 @@ public class Hangman {
 				System.out.println("Last guess Goodluck");
 			}
 			System.out.print("Enter another Guess: ");
-			Guess = input.next().charAt(0);
+			Guess = input.next().toUpperCase().charAt(0);
+
+			//insert HINT ability right here;
+
 
 			System.err.println(comp1 + " " + comp2);
 
